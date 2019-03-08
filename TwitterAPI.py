d639f5b8a6cdd023a6a032544b2f1dd45431a97c
Karan Pratap Singh <synester.gates4@gmail.com>
2019-03-08 16:21:45 +0530

    tweepy integrated

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -4,10 +4,9 @@ from tweepy.streaming import StreamListener
 import json
 
 # Authentication Key's for Twitter API
-consumer_key = '[consumer_key]'
-consumer_key_secret = '[consumer_key_secret]'
-access_token = '[access_token]'
-access_token_secret = '[access_token_secret]'
+from keys.twitter import twitter_auth
+
+consumer_key, consumer_key_secret, access_token, access_token_secret = twitter_auth.values()
 
 '''
  StreamTwitterAPI:
@@ -15,18 +14,20 @@ access_token_secret = '[access_token_secret]'
  This class inherits StreamListener class from tweepy's streaming
  module to stream twitter tweets
 '''
+
 class StreamTwitterAPI(StreamListener):
 
     def on_data(self, data):
-
-	    all_data = json.loads(data)
-	    tweet = all_data['text']
+        all_data = json.loads(data)
+        tweet = all_data['text']
+        print(tweet)
 
     def on_error(self, status):
         print(status)
+
 
 auth = OAuthHandler(consumer_key, consumer_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 twitterStream = Stream(auth, StreamTwitterAPI())
-twitterStream.filter(track=["happy"])
+twitterStream.filter(track=['donald trump'])
