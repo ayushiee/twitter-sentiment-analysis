e8989ad964d77f80cc1968896cc43fdc3231db64
Karan Pratap Singh <synester.gates4@gmail.com>
2019-03-08 15:44:08 +0530

    added requirements

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -1,4 +1,7 @@
-
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
 
 # Authentication Key's for Twitter API
 consumer_key = '[consumer_key]'
@@ -6,4 +9,29 @@ consumer_key_secret = '[consumer_key_secret]'
 access_token = '[access_token]'
 access_token_secret = '[access_token_secret]'
 
+'''
 
+'''
+class Listener(StreamListener):
+
+    def on_data(self, data):
+
+	    all_data = json.loads(data)
+	    tweet = all_data['text']
+		# sentiment_value, confidence = s.sentiment(tweet)
+		# print(tweet, sentiment_value, confidence)
+
+		# if confidence*100 >= 80:
+		# 	output = open('twitter-out.txt','a')
+		# 	output.write(sentiment_value)
+		# 	output.write('\n')
+		# 	output.close()
+
+    def on_error(self, status):
+        print(status)
+
+auth = OAuthHandler(consumer_key, consumer_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+twitterStream = Stream(auth, Listener())
+twitterStream.filter(track=["happy"])
