816cde205e27791d96a905c350895968a8b781a7
Karan Pratap Singh <synester.gates4@gmail.com>
2019-03-08 15:43:07 +0530

    keys for twitter api

---
+++ TwitterAPI.py
@@ -0,0 +1,9 @@
+
+
+# Authentication Key's for Twitter API
+consumer_key = '[consumer_key]'
+consumer_key_secret = '[consumer_key_secret]'
+access_token = '[access_token]'
+access_token_secret = '[access_token_secret]'
+
+
