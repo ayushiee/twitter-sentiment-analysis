d471a3e09f8414f78174b3035aeed063268a3509
Karan Pratap Singh <synester.gates4@gmail.com>
2019-03-08 17:26:31 +0530

    Twitter api stream class completer'

--- TwitterApi/TwitterApiStream.py
+++ TwitterApi/TwitterApiStream.py
@@ -1,5 +1,12 @@
-from tweepy.streaming import StreamListener
 import json
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+from TwitterApi.AuthenticationTokens import twitter_auth_tokens
+
+
+consumer_key, consumer_key_secret, access_token, access_token_secret = twitter_auth_tokens.values()
+
 
 class TwitterApiStream(StreamListener):
 
@@ -10,10 +17,22 @@ class TwitterApiStream(StreamListener):
     module to stream twitter tweets
     '''
 
-    # def __init__(self):
+    def __init__(self):
+        self.authentication = None
 
-    def set_auth_tokens(self, token):
-        print(token)
+    def startStreaming(self):
+        """
+        Starts the twitter streaming
+
+        Params:
+            self: self
+        """
+
+        self.authentication = OAuthHandler(consumer_key, consumer_key_secret)
+        self.authentication.set_access_token(access_token, access_token_secret)
+
+        Stream(self.authentication, self).filter(
+            track=['donald trump'], is_async=True)
 
     def on_data(self, data):
         """
@@ -22,7 +41,8 @@ class TwitterApiStream(StreamListener):
         Params:
             self: self
             data: json response
-        """   
+        """
+
         parsed_data = json.loads(data)
         tweet = parsed_data['text']
         print(tweet)
--- TwitterSentiment.py
+++ TwitterSentiment.py
@@ -1,14 +1,5 @@
-from tweepy import Stream
-from tweepy import OAuthHandler
-
 from TwitterApi.TwitterApiStream import TwitterApiStream
-from keys.twitter import twitter_auth
 
-consumer_key, consumer_key_secret, access_token, access_token_secret = twitter_auth.values()
+TwitterApiStream().startStreaming()
 
-
-auth = OAuthHandler(consumer_key, consumer_key_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-twitterStream = Stream(auth, TwitterApiStream())
-twitterStream.filter(track=['donald trump'])
+print('hello')
