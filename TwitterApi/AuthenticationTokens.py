f8c6c3a9935d521a254cce9cc42b77312723bfc3
Karan Pratap Singh <synester.gates4@gmail.com>
2019-05-07 18:12:29 +0530

    added tokens

---
+++ TwitterApi/AuthenticationTokens.py
@@ -0,0 +1,7 @@
+twitter_auth_tokens = {
+
+    'consumer_key': 'aF46YS1MdwQEB8y8xdyWQtthf',
+    'consumer_key_secret': 'EE7hjetNfOhbYcXEfZ7cQFhwv5wQznVQzXtfYQKZanHDDCnjtJ',
+    'access_token': '1103963306161668097-mL7569dfSP6OOKj4vhiNSqHeV6P8gg',
+    'access_token_secret': 'Gcu7iSldCKqk3CxRc9Hy3YpEgqTVp36Lpk6lntXeJCVZo'
+}
